200580866a0d88e10de8964d579ad42a0c0b0a41
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-09-30 23:06:43 +0300

    Update create.bat

--- create.bat
+++ create.bat
@@ -3,23 +3,23 @@ md %1
 cd %1
 md Algebra_and_Geometry
 cd Algebra_and_Geometry
-echo Ïîãîæåâ Ñåðãåé Âëàäèìèðîâè÷ >> readme.txt
-echo Íà÷àëî çàíÿòèé:%date% >> readme.txt
+echo ÃÃ®Ã£Ã®Ã¦Ã¥Ã¢ Ã‘Ã¥Ã°Ã£Ã¥Ã© Ã‚Ã«Ã Ã¤Ã¨Ã¬Ã¨Ã°Ã®Ã¢Ã¨Ã· >> readme.txt
+echo ÃÃ Ã·Ã Ã«Ã® Ã§Ã Ã­Ã¿Ã²Ã¨Ã©:%date% >> readme.txt
 cd ..
 md Math_analiz
 cd Math_analiz
-echo Ïëàòîíîâ Àëåêñåé Âèêòîðîâè÷ >> readme.txt
-echo Íà÷àëî çàíÿòèé:%date% >> readme.txt
+echo ÃÃ«Ã Ã²Ã®Ã­Ã®Ã¢ Ã€Ã«Ã¥ÃªÃ±Ã¥Ã© Ã‚Ã¨ÃªÃ²Ã®Ã°Ã®Ã¢Ã¨Ã· >> readme.txt
+echo ÃÃ Ã·Ã Ã«Ã® Ã§Ã Ã­Ã¿Ã²Ã¨Ã©:%date% >> readme.txt
 cd ..
 md Osnov_prog
 cd Osnov_prog
-echo Ïîãîæåâ Ñåðãåé Âëàäèìèðîâè÷ >> readme.txt
-echo Íà÷àëî çàíÿòèé:%date% >> readme.txt
+echo ÃÃ®Ã£Ã®Ã¦Ã¥Ã¢ Ã‘Ã¥Ã°Ã£Ã¥Ã© Ã‚Ã«Ã Ã¤Ã¨Ã¬Ã¨Ã°Ã®Ã¢Ã¨Ã· >> readme.txt
+echo ÃÃ Ã·Ã Ã«Ã® Ã§Ã Ã­Ã¿Ã²Ã¨Ã©:%date% >> readme.txt
 cd ..
 md Diskret_math
 cd Diskret_math
-echo Ïîãîæåâ Ñåðãåé Âëàäèìèðîâè÷ >> readme.txt
-echo Íà÷àëî çàíÿòèé:%date% >> readme.txt
+echo ÃÃ®Ã£Ã®Ã¦Ã¥Ã¢ Ã‘Ã¥Ã°Ã£Ã¥Ã© Ã‚Ã«Ã Ã¤Ã¨Ã¬Ã¨Ã°Ã®Ã¢Ã¨Ã· >> readme.txt
+echo ÃÃ Ã·Ã Ã«Ã® Ã§Ã Ã­Ã¿Ã²Ã¨Ã©:%date% >> readme.txt
 cd ..\..
-copy algem.bat semestr1
-copy op.bat semestr1
+copy algem.bat %1
+copy op.bat %1
