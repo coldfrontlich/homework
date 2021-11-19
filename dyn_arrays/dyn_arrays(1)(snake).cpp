38cbb0c897d2afc4dca3eda2744ee68e10d9bde9
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-11-20 00:10:53 +0300

    Add files via upload

---
+++ dyn_arrays/dyn_arrays(1)(snake).cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main() {
+    int n, m;
+    do
+    {
+        std::cout << "Input number of rows: ";
+        std::cin >> n;
+    } while (n < 1);
+
+    do
+    {
+        std::cout << "Input number of colums: ";
+        std::cin >> m;
+    } while (m < 1);
+
+    int** M = new int* [n];
+
+    for (int i = 0; i < n; ++i) {
+        M[i] = new int[m];
+    }
+
+    int num = 1;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if ((i % 2 == 0) and (j == m - 1)) {
+                M[i][j] = num;
+                num += m;
+            }
+            else if (i % 2 == 0) {
+                M[i][j] = num;
+                num++;
+            }
+            else if ((i % 2 != 0) and (j == m - 1)) {
+                M[i][j] = num;
+                num += m;
+            }
+            else if (i % 2 != 0) {
+                M[i][j] = num;
+                num--;
+            }
+
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cout << M[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < n; ++i){
+        delete[] M[i];
+    }
+
+    delete[] M;
+}
