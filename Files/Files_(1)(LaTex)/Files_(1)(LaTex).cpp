1e1faa4534015e0243c417ec9a8565b7bc8e6089
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-12-16 20:56:53 +0300

    Add files via upload

---
+++ Files/Files_(1)(LaTex)/Files_(1)(LaTex).cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, const char* argv[])
+{
+
+    if (argc < 3)
+    {
+        std::cout << "Wrong input\n";
+        std::cout << "Usage: Files inFile_name, outFile_name\n";
+        return 1;
+    }
+    std::ifstream in(argv[1]);
+    if (!in.is_open())
+    {
+        std::cout << "Input file open error \n";
+        return 2;
+    }
+    else {
+        std::cout << "Input file opened\n";
+    }
+    std::ofstream out(argv[2]);
+    if (!out.is_open())
+    {
+        std::cout << "Output file open error \n";
+        return 3;
+    }
+    else {
+        std::cout << "Output file opened\n";
+    }
+    char c;
+    int k = 0;
+    while ((c = in.get()) != EOF)
+    {
+        if ((c == '$') and (in.peek() == '$') and (k % 2 == 0))
+        {
+            out << "<math>";
+            c = in.get();
+            c = in.get();
+            ++k;
+        }
+        else if ((c == '$') and (in.peek() == '$') and (k % 2 != 0))
+        {
+            out << "</math>";
+            c = in.get();
+            c = in.get();
+            ++k;
+        }
+        if ((c == '$') and (in.peek() != '$') and (k % 2 == 0))
+        {
+            out << "<math>";
+            c = in.get();
+            ++k;
+        }
+        else if ((c == '$') and (in.peek() != '$') and (k % 2 != 0))
+        {
+            out << "</math>";
+            c = in.get();
+            ++k;
+        }
+        out << c;
+    }
+    out.close();
+    in.close();
+    std::cout << "Files are closed successfully";
+    return 0;
+}
