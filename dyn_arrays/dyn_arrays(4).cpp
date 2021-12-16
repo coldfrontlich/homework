ed2fd7c974f784fd00719876c12e74def572b755
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-12-16 16:37:33 +0300

    Update dyn_arrays(4).cpp

--- dyn_arrays/dyn_arrays(4).cpp
+++ dyn_arrays/dyn_arrays(4).cpp
@@ -1,3 +1,6 @@
+//Написать функцию нахождения корня из числа с помощью метода Ньютона double mysqrt(double x, double eps=1e-6);
+//Вычисление осуществляется по формуле: $Z_{n+1} = Z_n - \frac{Z_n^2-x}{2 Z_n}$, $Z_0 = 1.0$.
+//Вычислять пока $|Z_{n+1} - Z_{n}|>\epsilon$.
 #include <iostream>
 #include <cmath>
 
