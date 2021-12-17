c182979777c558aaeba6c457051fd968fa8baa1b
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-12-17 19:53:11 +0300

    Create Tribonacci Sequence.cpp

---
+++ codewars/Tribonacci Sequence.cpp
@@ -0,0 +1,16 @@
+std::vector<int> tribonacci(std::vector<int> signature, int n){
+    if (n == 0){
+    std::vector<int> result0; 
+    return result0;
+    } else {
+    std::vector<int> result(n);
+    result [0] += signature [0];
+    result [1] += signature [1];
+    result [2] += signature [2];
+    for (int i = 3; i < n; ++i)
+    {
+        result[i] += result[i-3] + result[i-2] + result[i-1];
+    }
+    return result;
+    }
+}
