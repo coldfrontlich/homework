4f57b5b22b26c353ab0d16e79c1e883189a2e54b
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-12-27 21:21:05 +0300

    Update Reverse words.cpp

--- codewars/Reverse words.cpp
+++ codewars/Reverse words.cpp
@@ -1,4 +1,4 @@
-std::string reverse_words(std::string str)
+/*std::string reverse_words(std::string str)
 {
   std::string revers;
   std::string result;
@@ -14,4 +14,39 @@ std::string reverse_words(std::string str)
   }
   result += revers;
   return result;
+}*/
+#include <iostream>
+#include <string>
+
+void sw(char& a, char& b)
+{
+    char tmp = a;
+    a = b;
+    b = tmp;
+}
+
+std::string reverse_words(std::string str)
+{
+    int ind_1 = 0;
+    int ind_2 = 0;
+    int lenn = str.length();
+
+    while (ind_2 < lenn)
+    {
+        while (str[ind_1] == ' '){
+          ++ind_1;
+        }
+
+        while ((ind_2 != lenn - 1) and (str[ind_2 + 1] != ' ')){
+          ++ind_2;
+        }
+
+        for (int i = ind_1; i < ind_1 + (ind_2 - ind_1 + 1)/(2); ++i){
+          sw(str[i], str[ind_2 - (i - ind_1)]);
+        }
+
+        ind_1 = ++ind_2;
+    }
+
+    return str;
 }
