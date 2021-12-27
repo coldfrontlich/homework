d40c458e0365b9f3405c848adb3a4c98d1024b9b
Maxim Pomogaev <91350541+coldfrontlich@users.noreply.github.com>
2021-12-27 17:02:10 +0300

    Update sail.cpp

--- sail.cpp
+++ sail.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <cmath>
 
-bool first(double x, double y) {
-    return (x >= 0 and y >= 0 and y <= -2 * (x - 1));
+bool triangles(double x, double y)
+{
+    return ((x >= -1) and (x <= 1) and (y >= 0) and (y <= -2 * abs(x) + 2));
 }
-bool second(double x, double y) {
-    return (x <= 0 and y >= 0 and y <= 2 * (x + 1));
+
+bool third(double x, double y)
+{
+    return ((y <= 0) and (x >= -2) and (x <= 0) and (y >= -sqrt(1 - (x + 1) * (x + 1))));
 }
-bool third(double x, double y) {
-    return (x >= 0 and y <= 0 and y >= -sqrt(1 - pow((x + 1), 2)));
+
+bool fourth(double x, double y)
+{
+    return ((y <= 0) and (x <= 2) and (x >= 0) and (y >= -sqrt(1 - (x - 1) * (x - 1))));
 }
-bool fourth(double x, double y) {
-    return (x >= 0 and y >= 0 and y >= -sqrt(1 - pow((x - 1), 2)));
-}
+
+
 int main() {
     double x, y;
-    std::cout << "Input x,y: " << std::endl;
+    std::cout << "Input x and y: ";
     std::cin >> x >> y;
-    if (first(x, y) or second(x, y) or third(x, y) or fourth(x, y)) {
+    if (third(x, y) or fourth(x, y) or triangles(x, y))
+    {
         std::cout << "Popadaet" << std::endl;
     }
-    else {
+    else
+    {
         std::cout << "Ne popadaet" << std::endl;
     }
 }
